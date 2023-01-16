9d9e8fb357664ff82ff672d4eaaa36dac152ef63
yuki-iwase <mischief.moi@gmail.com>
2023-01-17 03:57:09 +0900

    簡単なアロー関数を作成

--- functionSample.js
+++ functionSample.js
@@ -4,3 +4,10 @@ function add1(v1, v2) {
 
 const result1 = add1(1, 2);
 console.log(`reseult1 = ${result1}`);
+
+const add2 = (v1, v2) => {
+  return v1 + v2;
+}
+
+const result2 = add1(1, 2);
+console.log(`reseult2 = ${result2}`);
